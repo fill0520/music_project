7036d589fedf30a62dad5a3f1c6572c660bfc367
Elnard Utiushev <elnardu2@gmail.com>
2017-02-06 20:54:02 +0600

    Initial add

---
+++ move_everything.py
@@ -0,0 +1,18 @@
+import os, re
+
+path = os.path.dirname(os.path.abspath(__file__)) + '/'
+mp3regex = re.compile('.*mp3')
+
+onlymusic = [f for f in os.listdir(path) if mp3regex.match(f)]
+for song in onlymusic:
+	folder = path + song[:-4]
+	os.mkdir(folder)
+	os.rename(path+song, folder+'/'+song)
+
+musicdirs = [f[0] for f in os.walk(path)]
+musicdirs = musicdirs[1:]
+for d in musicdirs:
+	file = d + "/README.md"
+	with open(file, 'w') as f:
+		f.write('#Топовые моменты и тайминг:')
+
